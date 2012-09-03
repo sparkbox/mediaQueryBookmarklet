2625ecb53ac949315413f448a01b1aa5b3662e1b
Rob Tarr <rob@robtarr.net>
2012-09-03 18:19:29 -0400

    Bumping version number

--- js/mediaQueryBookmarklet.js
+++ js/mediaQueryBookmarklet.js
@@ -9,7 +9,7 @@ window.mqb = {
       document.body.removeChild( bookmarklet );
     }
 
-    mqb.version = '1.4.1';
+    mqb.version = '1.4.2';
     mqb.tmpl =
       "<p id=\"mqb-dimensions\"></p>" +
       "<p id=\"mqb-mousePosition\"></p>" +
